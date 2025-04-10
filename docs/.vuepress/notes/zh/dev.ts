4bf873c1abb6963d15d7fde1150b4adda951e24b
weiliudong <24930918+weiliudong@users.noreply.github.com>
2025-04-10 15:23:17 +0800

    update

--- docs/.vuepress/notes/zh/dev.ts
+++ docs/.vuepress/notes/zh/dev.ts
@@ -8,20 +8,24 @@ export default defineNoteConfig({
             text: '基础服务',
             collapsed: false,
             items: [
-                '说明', 
+                '说明',
                 'llm',
                 'asr',
                 'tts',
                 '相同语义'
             ],
-        }, 
+        },
         {
             text: 'ESP-AI 开放接口',
             collapsed: false,
             items: [
                 "知识库",
-                '开发中',  
+                "额度卡接口",
+                "额度支付接口",
+                "插件配置接口",
+                "设备管理接口",
+                '开发中',
             ],
-        }, 
+        },
     ],
 })
