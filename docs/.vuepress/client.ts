a9b3527f3262b664c35be997c96036cac4914208
tianyuefei <aa1127z@163.com>
2025-08-04 15:21:53 +0800

    fix：儿童方案问题修复

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,7 +1,11 @@
 import { defineClientConfig } from 'vuepress/client'
 
+import Toy from "./layouts/toy/index.vue";
+
 import './styles/index.scss'
 
 export default defineClientConfig({
-  // ...
+  layouts: {
+    Toy
+  },
 })
