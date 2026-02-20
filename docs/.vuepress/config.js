0d8050ed18d24e37db30948622c9f7a5c034ab2d
wangzongming <1746809408@qq.com>
2026-02-20 21:55:13 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -144,7 +144,7 @@ export default defineUserConfig({
 
         bulletin: {
             layout: 'center', 
-            id: '20',
+            id: '22',
             lifetime: 'once',
             title: '🎉 ESP-AI 公告 🎉',
             contentFile: path.join(__dirname, 'bulletin.md'),
